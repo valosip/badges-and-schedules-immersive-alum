d906832b56fb7f24e9064612d8cddbd574d5ded0
Val Osip <valosip@gmail.com>
2020-04-04 23:02:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(array)
   result = []
   array.each do |name|
-    result << "Hello, my name is #{#name}."
+    result << "Hello, my name is #{name}."
   end
   result
 end
